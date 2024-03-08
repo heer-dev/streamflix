fd4a91e251a9826c837a9ba78fc43a90b172d7e1
Heer Patel <heervippatel@gmail.com>
2024-03-08 17:17:32 -0500

    responsive

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,42 +1,35 @@
 import React, { useState } from 'react';
 import RegistrationForm from './RegistrationForm';
 import LoginForm from './LoginForm';
-import "../assets/App.css"
+import "../assets/App.css";
 import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   const [showForm, setShowForm] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleShowRegistration = () => {
-    setShowForm('register');
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleShowLogin = () => {
-    setShowForm('login');
-  };
-
-  const handleClose = () => {
-    setShowForm(null);
-  };
+  const handleShowRegistration = () => setShowForm('register');
+  const handleShowLogin = () => setShowForm('login');
+  const handleClose = () => setShowForm(null);
 
   return (
-    <header ckassName="header">
-    <nav className="navigation">
-      <Link to='/' key="home">Home</Link>
-      <Link to='/MovieListing' key="movies">Movies</Link>
-      <Link to='/ShowListing' key="tvShows">TvShows</Link>
-      
+    <header className="header">
+      <div className="hamburger-menu" onClick={toggleMenu}>☰</div>
+      <nav className={`navigation ${isMenuOpen ? 'show' : ''}`}>
+        <Link to="/" key="home">Home</Link>
+        <Link to="/MovieListing" key="movies">Movies</Link>
+        <Link to="/ShowListing" key="tvShows">TvShows</Link>
+      </nav>
       <div className='right-nav'>
-      <button onClick={handleShowRegistration}>SignUp</button>
-      <button onClick={handleShowLogin}>SignIn</button>
+        <button onClick={handleShowRegistration}>SignUp</button>
+        <button onClick={handleShowLogin}>SignIn</button>
       </div>
-      {showForm === 'register' && (
-        <RegistrationForm onClose={handleClose} />
-      )}
-      {showForm === 'login' && (
-        <LoginForm onClose={handleClose} />
-      )}
-   </nav>
+      {showForm === 'register' && <RegistrationForm onClose={handleClose} />}
+      {showForm === 'login' && <LoginForm onClose={handleClose} />}
     </header>
   );
 };
--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -6,6 +6,7 @@ const MovieDetailsPage = () => {
     <div>
       <Header/>
       <MovieDetails/>
+      
     </div>
   )
 }
