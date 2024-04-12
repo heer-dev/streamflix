4c678e9aff4e634efaa1dda890627bcbe90db1ed
Heer Patel <heervippatel@gmail.com>
2024-04-12 13:17:20 -0400

    Resolved merge conflicts

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -11,7 +11,7 @@ const LogIn = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('https://streamflic-backend-827287f6aade.herokuapp.com/media/featured?type=tvshows/customers/authenticate', {
+        const response = await fetch('https://streamflic-backend-827287f6aade.herokuapp.com/customers/authenticate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
